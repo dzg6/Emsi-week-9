9bdf2697c73884892b8a98a5e2a29496edd0f8eb
dzg6 <mdr.photoshop@gmail.com>
2021-05-04 15:03:46 -0700

    final form

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ document.getElementById('datePicker').value = new Date().toLocaleDateString('en-
 document.getElementById("createUser").addEventListener("submit", createUser);
 document.getElementById("transaction").addEventListener("submit", transaction);
 
+var users = [];
 
-function createUser (e){
+
+//submit function for user form
+function createUser(e) {
     e.preventDefault();
     const form = new FormData(e.target);
 
@@ -22,21 +25,32 @@ function createUser (e){
             "Content-Type": "application/json"
         },
         "body": JSON.stringify(payload)
-    }; 
-    fetchCall(url, request)    
+    };
+    fetchCall(url, request)
+        .then(data => {
+            if (data.user) {
+                users.push(form.get('user'))
+                document.getElementById('createUser').reset();
+                updateUsers();
+                console.log('user created')
+                console.log(data)
+            } else {
+                console.log(data.error)
+            }
+        });
 };
 
-
-function transaction (e){
+//submit function for transaction form
+function transaction(e) {
     e.preventDefault();
-    const form = new FormData(e.target); 
-    let user =  form.get('user');
+    const form = new FormData(e.target);
+    let user = form.get('user');
     let payload = {
         date: form.get('date'),
         object: form.get('object'),
         amount: form.get('amount'),
     }
-    
+
     let url = 'http://localhost:5000/api/accounts/' + user + '/transactions';
     let request = {
         "method": 'POST',
@@ -44,20 +58,36 @@ function transaction (e){
             "Content-Type": "application/json"
         },
         "body": JSON.stringify(payload)
-    }; 
-    fetchCall(url, request)  
+    };
+    fetchCall(url, request)
+        .then(data => {
+            if(!data.error){
+            document.getElementById('transaction').reset();
+            console.log('transaction created')
+            console.log(data)
+            }else{
+                console.log(data)
+            }
+        });
 };
 
 
+//Creates all the user options for data List via transactions form
+function updateUsers() {
+    users.forEach(user => {
+        var optionElement = document.createElement("option");
+        optionElement.value = user;
+        document.getElementById("allUsers").appendChild(optionElement);
 
+    });
+}
 
-
-function fetchCall(url, req){
+function fetchCall(url, req) {
 
     return fetch(url, req)
-    .then(response => response.json())
-    .then(data => { return data })
-    .catch(err => {
-        console.error(err);
-    });
+        .then(response => response.json())
+        .then(data => { return data })
+        .catch(err => {
+            console.error(err);
+        });
 };
